07e8bead66427636a14e2597a0f0a9cbdfa1b502
lorenapinkala <baeksumin77@gmail.com>
2023-02-10 10:02:39 -0300

    inicial files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+//const models = require("./models/index");
+//const db = require("./db");
+//const router = require("./routes");
+//require('dotenv').config();
+// logging middleware
+app.use(morgan("tiny"));
+
+// parsing middleware
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors());
+
+//app.use("/api", router);
+
+// error middleware -> https://expressjs.com/es/guide/error-handling.html
+app.use((err, req, res, next) => {
+  console.log("ERROR");
+  console.log(err);
+  res.status(500).send(err.message);
+});
+
+
+
+  app.listen(3000, () => {
+    console.log("Servidor escuchando en el puerto 3000");
+  });
